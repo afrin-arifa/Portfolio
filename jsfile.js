9198e8fa62d3628bb8355b9f56b435e6ac01767f
Arifa Afrin <arifaafrinar@gmail.com>
2024-12-23 01:06:14 +0600

    Initial commit

---
+++ jsfile.js
@@ -0,0 +1,33 @@
+var typed = new Typed('#element', {
+	strings: ['Web Developer.', 'Graphic Designer.', 'Web Designer.', 'App Developer.'],
+	typeSpeed: 30,
+  });
+
+//   Skill progress BarProp.
+  var skillPers = document.querySelectorAll(".skill-per");
+
+  skillPers.forEach(function(skillPer) {
+	var per = parseFloat(skillPer.getAttribute("per"));
+	skillPer.style.width = per + "%";
+	
+	var animatedValue = 0; 
+	var startTime = null;
+	
+	function animate(timestamp) {
+	  if (!startTime) startTime = timestamp;
+	  var progress = timestamp - startTime;
+	  var stepPercentage = progress / 1000; // Dividing by duration in milliseconds (1000ms = 1s)
+	  
+	  if (stepPercentage < 1) {
+		animatedValue = per * stepPercentage;
+		skillPer.setAttribute("per", Math.floor(animatedValue) + "%");
+		requestAnimationFrame(animate);
+	  } else {
+		animatedValue = per;
+		skillPer.setAttribute("per", Math.floor(animatedValue) + "%");
+	  }
+	}
+	
+	requestAnimationFrame(animate);
+  });
+  
